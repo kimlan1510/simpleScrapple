a76e3d0a17bc4ae3d38b3e786f3f7c45c10da7fb
Ethan Luts and kimlan nguyen <ethan+kimlan@>
2017-06-01 08:20:56 -0700

    Create initial files

---
+++ Modules/HomeModule.cs
@@ -0,0 +1,13 @@
+using Nancy;
+using System.Collections.Generic;
+using Scrapple.Objects;
+using System;
+
+namespace Scrapple
+{
+  public class HomeModule: NancyModule
+  {
+    public HomeModule()
+    {
+      
+    }
---
+++ Objects/Score.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Colellections.Generic;
+
+namespace Scrapple.Objects
+{
+  public class ScrappleWord
+  {
+    private int _point;
+    private string _word;
+    public ScrapleWord(string word)
+    {
+
+    }
+  }
+}
